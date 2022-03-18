f978ce29f100ef3017d24c76fa99a9682ff33d23
James D <jimdavies72@googlemail.com>
2022-03-18 09:10:07 +0000

    Intiail boiler plate commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import { Test } from "./components/test/Test";
+
+const App = () => {
+  const [user, setUser] = useState();
+
+  const clearUserHandler = () => {
+    setUser(null);
+    localStorage.removeItem(process.env.REACT_APP_LSTOKEN);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/fetchDry.js
@@ -0,0 +1,47 @@
+export const fetchRequest = async (endpoint, payload, httpVerb) => {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_REST_API}${endpoint}`,
+      {
+        method: httpVerb,
+        headers: { "Content-Type": "application/json" },
+        body: payload ? JSON.stringify(payload) : null,
+      }
+    );
+
+    console.log(response);
+    const data = await response.json();
+
+    if (data) {
+      return data;
+    } else {
+      throw new Error(`${response.status}, ${response.message}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const tokenizedFetch = async (endpoint, payload, httpVerb) => {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_REST_API}${endpoint}`,
+      {
+        method: httpVerb,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("m34Token")}`,
+        },
+        body: payload ? JSON.stringify(payload) : null,
+      }
+    );
+    const data = await response.json();
+    if (data) {
+      return data;
+    } else {
+      throw new Error(`${response.status}, ${response.message}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
