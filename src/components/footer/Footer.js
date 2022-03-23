52249f22c8de0eb95bbb292258669ef968ac91a9
EethanF <ethan_frediani@yahoo.co.uk>
2022-03-23 11:38:45 +0000

    footer layout complete

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,31 @@
+import React from "react";
 import "./Footer.css";
+import logo from "../../media/logo-1.png"; 
 
 const Footer = () => {
   return (
     <div className="container">
-      <h1>Footer Component</h1>
+      <div className="row">
+        <div className="git">
+          <h2>Creators</h2>
+          <ul className="list">
+            <li><a href="https://github.com/CatTiac">Cait J</a></li>
+            <li><a href="https://github.com/jimdavies72">James D</a></li>
+            <li><a href="https://github.com/clarkenewsh">Clarke N</a></li>
+            <li><a href="https://github.com/EethanF">Ethan F</a></li>
+            <li><a href="https://github.com/ConnahShackley">Connah S</a></li>
+          </ul>
+        </div>
+        <div className="aboutUs">
+          <h2>About Us</h2>
+          <p>As students at CodeNation we where tasked with building a web app. We decided, as a team, to create something
+            that would help people with missing loved ones.</p>
+        </div>
+        <div className="footerLogo">
+          <img id="footerLogoImg" src={logo} alt=""/>
+          <p className="copyright">© 2022-2023 ICQ.</p>
+        </div>
+      </div>
     </div>
   );
 };
