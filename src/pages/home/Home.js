e156d8b25f7a61ad7b8d094355d46226599b2730
Cait J <95374937+CatTiac@users.noreply.github.com>
2022-03-23 11:13:09 +0000

    cj - Home Page Component - Updated index.js with setUser

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,19 @@
+import MissingPersonList from "../../components/missingPersonList/MissingPersonList";
+import { SearchBar } from "../../components/searchBar";
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <section className="HomeContainer">
+        <section className="SearchContainer">
+          <SearchBar />
+          <button className="ButtonMain">My Missing Person</button>
+          <button className="ButtonMain">Register Missing Person</button>
+        </section>
+        <h1>All Missing People</h1>
+        <MissingPersonList />
+      </section>
     </>
   );
 };
