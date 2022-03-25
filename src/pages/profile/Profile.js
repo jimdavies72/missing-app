867769b0d1f1a45f3061c26fe51746cda8f2e3f0
EethanF <ethan_frediani@yahoo.co.uk>
2022-03-25 15:24:23 +0000

    css changes to profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,9 +1,9 @@
-import "./Profile.css";
 import { Navigate } from "react-router-dom";
 
 import UpdateProfile from "../../components/updateProfile/UpdateProfile";
 import UpdatePassword from "../../components/updatePassword/UpdatePassword";
 import DeleteUser from "../../components/deleteUser/DeleteUser";
+import "./Profile.css";
 
 const Profile = ({ user, setUser, clearUserHandler }) => {
   return (
@@ -11,10 +11,11 @@ const Profile = ({ user, setUser, clearUserHandler }) => {
       {!user && <Navigate to="/" />}
 
       <h2>{user}'s Profile Page</h2>
-
-      <UpdateProfile user={user} setUser={setUser} />
-      <UpdatePassword user={user} setUser={setUser} />
-      <DeleteUser user={user} clearUserHandler={clearUserHandler} />
+      <div className ="updateForm">
+        <UpdateProfile user={user} setUser={setUser} />
+        <UpdatePassword user={user} setUser={setUser} />
+        <DeleteUser user={user} clearUserHandler={clearUserHandler} />
+      </div>
     </div>
   );
 };
