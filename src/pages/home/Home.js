f5c1cacc2c24c39e264c24cc252ed098e990a2a6
Cait J <95374937+CatTiac@users.noreply.github.com>
2022-03-29 13:39:08 +0100

    added title banner on home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 // import { listMissingPeople } from "../../utils/index.js";
 import MissingPersonList from "../../components/missingPersonList/MissingPersonList.js";
 import SearchBar from "../../components/searchBar/SearchBar.js";
+import backgroundBannerCloudL from "../../media/backgroundBannerCloudL.png";
 import "./Home.css";
 
 const Home = ({ user, updatePersonHandler, createPersonHandler }) => {
@@ -22,7 +23,23 @@ const Home = ({ user, updatePersonHandler, createPersonHandler }) => {
   return (
     <>
       {!user && <Navigate to="/" />}
-      <section>
+      <section className="home-container">
+        <div
+          className="title-container"
+          style={{
+            // backgroundImage: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.3)), url(${backgroundBannerCloudL})`,
+            // backgroundImage: `linear-gradient(90deg, rgba(36,123,160,1) 0%, rgba(27,152,224,1) 20%, rgba(255,255,255,1) 100%), url(${backgroundBannerCloudL})`,
+            // backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${backgroundBannerCloudL})`,
+            // backgroundImage: `linear-gradient(rgba(200, 200, 255, 0.5), rgba(200, 200, 255, 0.5)), url(${backgroundBannerCloudL})`,
+            // backgroundImage: `linear-gradient(rgba(155, 155, 155, 0.5), rgba(155, 155, 155, 0.5)), url(${backgroundBannerCloudL})`,
+            backgroundImage: `linear-gradient(rgba(36,123,160,0.25), rgba(36,123,160,0.25)), url(${backgroundBannerCloudL})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            borderRadius: "0.375rem",
+          }}
+        >
+          <h1>Independent Concern Query</h1>
+        </div>
         <section className="search-container">
           <div>
             <SearchBar
@@ -31,29 +48,29 @@ const Home = ({ user, updatePersonHandler, createPersonHandler }) => {
             />
           </div>
           <div>
-          {bool ? (
-            <button
-              onClick={() => filterOriginHandler()}
-              className="button-main"
-            >
-              My Missing Persons
-            </button>
-          ) : (
-            <button
-              onClick={() => filterOriginHandler()}
-              className="button-main"
-            >
-              All Missing Persons
-            </button>
-          )}
-          <Link to="/register">
-            <button
-              onClick={() => createPersonHandler(true)}
-              className="button-main"
-            >
-              Register Missing Person
-            </button>
-          </Link>
+            {bool ? (
+              <button
+                onClick={() => filterOriginHandler()}
+                className="button-main"
+              >
+                My Missing Persons
+              </button>
+            ) : (
+              <button
+                onClick={() => filterOriginHandler()}
+                className="button-main"
+              >
+                All Missing Persons
+              </button>
+            )}
+            <Link to="/register">
+              <button
+                onClick={() => createPersonHandler(true)}
+                className="button-main"
+              >
+                Register Missing Person
+              </button>
+            </Link>
           </div>
         </section>
         <MissingPersonList
